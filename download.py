dee04a8f42774161693ba0a8a6cd8dfbb6847699
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2022-10-29 20:37:10 -0400

    download.py now downloads all surveys since 2011

--- download.py
+++ download.py
@@ -5,31 +5,22 @@ import os
 from os import path
 import sys
 
-BASE_DIR = "stack-overflow-developer-survey-2022"
-ZIP_FILENAME = "data.zip"
+MAIN_DIR = "surveys"
+ZIP_FILENAME= "data.zip"
+dirs = [str(year) for year in range(2011, 2023)]
 
-# Create directory if it doesn't exist already
-if not path.exists(BASE_DIR):
-  os.mkdir(BASE_DIR)
+if not path.exists(MAIN_DIR):
+  os.mkdir(MAIN_DIR)
 else:
-  print(f"Error: Cannot download survey data directory \'{BASE_DIR}\' already exists.", file=sys.stderr)
+  print(f"Error: Cannot download survey data directory \'{MAIN_DIR}\' already exists.", file=sys.stderr)
   exit(1)
 
-# Download zip file
-res = requests.get("https://info.stackoverflowsolutions.com/rs/719-EMH-566/images/stack-overflow-developer-survey-2022.zip")
-
-zip_filepath = path.join(BASE_DIR, ZIP_FILENAME)
-
-open(zip_filepath, "wb").write(res.content)
-
-# Extract zip into folder
-with zipfile.ZipFile(zip_filepath) as zip_ref:
-  zip_ref.extractall(BASE_DIR)
-
-# Cleanup unneeded files
-os.remove(zip_filepath)
-os.remove(path.join(BASE_DIR, "README_2022.txt"))
-os.remove(path.join(BASE_DIR, "so_survey_2022.pdf"))
-
-
-
+for dirname in dirs:
+  os.mkdir(path.join(MAIN_DIR, dirname))
+  url = f'https://info.stackoverflowsolutions.com/rs/719-EMH-566/images/stack-overflow-developer-survey-{dirname}.zip'
+  res = requests.get(url)
+  zip_filepath = path.join(MAIN_DIR, dirname, ZIP_FILENAME)
+  open(zip_filepath, "wb").write(res.content)
+  with zipfile.ZipFile(zip_filepath) as zip_ref:
+    zip_ref.extractall(path.join(MAIN_DIR, dirname))
+    os.remove(zip_filepath)
